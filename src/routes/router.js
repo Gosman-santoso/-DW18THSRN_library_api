6b191e30aec88286603cccd8562de1f6d70640df
Gosman-santoso <gosmansantoso18@gmail.com>
2020-10-08 22:29:07 +0700

    add Category API

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,7 +4,21 @@ const router = express.Router();
 
 const { getUsers, deleteUser } = require("../controller/userData.js");
 
+const {
+    getCategory,
+    detailCategory,
+    addCategory,
+    deleteCategory,
+    editCategory
+} = require("./../controller/categoryData.js");
+
 router.get("/users", getUsers);
 router.delete("/user/:id", deleteUser);
+
+router.get("/categories", getCategory);
+router.get("/category/:id", detailCategory);
+router.post("/categories", addCategory);
+router.patch("/category/:id", editCategory);
+router.delete("/category/:id", deleteCategory);
 
 module.exports = router;
